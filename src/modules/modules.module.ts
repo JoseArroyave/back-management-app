2bd6e8a48ead49f8b7d67f0fafe30190746d23db
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 14:41:26 -0500

    feat: update ModulesModule to include TasksModule and AuthModule for improved modularity

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,8 +1,10 @@
+import { TasksModule } from "./tasks/tasks.module";
+import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
 import { Module } from "@nestjs/common";
 
 @Module({
-  imports: [UsersModule],
-  exports: [UsersModule],
+  imports: [UsersModule, TasksModule, AuthModule],
+  exports: [UsersModule, TasksModule, AuthModule],
 })
 export class ModulesModule {}
