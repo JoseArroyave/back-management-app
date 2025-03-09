a3116dffccc7f4df5723527f72a78fb678674dfe
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 16:47:25 -0500

    refactor: remove unused mail transport environment variables from ProcessEnv type

--- src/common/types/process-env.d.ts
+++ src/common/types/process-env.d.ts
@@ -1,17 +1,11 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    MAIL_TRANSPORT: string;
     DB_CONNECTION: string;
-    MAIL_PASSWORD: string;
     DB_DATABASE: string;
     DB_USERNAME: string;
     DB_PASSWOR: string;
     JWT_SECRET: string;
     HASH_SALT: number;
-    MAIL_HOST: string;
-    MAIL_USER: string;
-    MAIL_FROM: string;
-    MAIL_PORT: number;
     NODE_ENV: string;
     DB_HOST: string;
     DB_PORT: number;
