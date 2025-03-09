1439e04af0928f9cb00e35340fc08f7faac493b8
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 00:51:39 -0500

    refactor: remove AppController and AppService, update AppModule for new structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DATA_SOURCE_CONFIG } from "./database/data.source";
+import { STORAGE_PATH } from "./common/constants/storage";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { ModulesModule } from "./modules/modules.module";
+import { AuthModule } from "./modules/auth/auth.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from "@nestjs/common";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...DATA_SOURCE_CONFIG,
+      retryAttempts: 3,
+      cache: false,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: STORAGE_PATH,
+      serveRoot: "/storage",
+    }),
+    ModulesModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
