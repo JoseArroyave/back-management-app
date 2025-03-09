f5c960ae6ebe64d76bb9bbc3a816045870cf92f3
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 00:52:22 -0500

    feat: add JWT authentication guard for enhanced security

---
+++ src/common/guards/jwt-auth.guard.ts
@@ -0,0 +1,6 @@
+import { Injectable, Global } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+
+@Global()
+@Injectable()
+export class JwtAuthGuard extends AuthGuard("jwt") {}
