f7e7c42c57d66554407e416626038752c4f2cc01
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 14:41:13 -0500

    feat: update IUsers interface to include tasks and change idUser type to string

--- src/modules/users/users.interface.ts
+++ src/modules/users/users.interface.ts
@@ -1,8 +1,10 @@
 import { IBase } from "../../common/interfaces/base.interface";
+import { TasksEntity } from "../tasks/tasks.entity";
 
 export interface IUsers extends IBase {
+  tasks: TasksEntity[];
   password: string;
-  idUser: number;
+  idUser: string;
   email: string;
   name: string;
 }
